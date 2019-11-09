0cb431d54a2099b3197ca35c5abbf6fe13d6567f
Andre Ferreira Lima <andre.lima@grupoltm.com.br>
2019-11-09 19:50:30 -0300

    kafka consumer

--- src/index.js
+++ src/index.js
@@ -13,11 +13,13 @@ var ProcessMessage = function(messageSet, topic, partition) {
 	messageSet.forEach(function(m) {
 		console.log(topic, partition, m.offset, m.message.value.toString('utf8'))
 
-		MovieSync({ id: 1, rate: 5 })
+		var message = JSON.parse(m.message.value.toString('utf8'))
+
+		MovieSync(partition, m.offset, message)
 	})
 }
 
-const MovieSync = async message => {
+const MovieSync = async (partition, offset, message) => {
 	let options = {
 		type: 'POST',
 		uri: 'movies-api.service.consul:8083/rate',
@@ -26,11 +28,25 @@ const MovieSync = async message => {
 		body: message
 	}
 	try {
-		let movie = await client(options)
-		return movie
+		//let movie = await client(options)
+
+		console.log(options)
+		Commit(partition, offset)
+
+		//return movie
 	} catch (error) {
 		throw MovieError(error)
 	}
+}
+
+const Commit = (partition, offset) => {
+	consumer.commitOffset([
+		{
+			topic: 'rating-topic',
+			partition: partition,
+			offset: offset
+		}
+	])
 }
 
 const Start = () =>
